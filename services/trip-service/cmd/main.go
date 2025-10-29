2734177627d74152734d21c816798b660f369c7e
Fuzail Ahmed <fuzail1280@gmail.com>
2025-10-29 05:29:40 +0000

    gracefully shutdown servers of the trip and gateway

--- services/trip-service/cmd/main.go
+++ services/trip-service/cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	h "ride-sharing/services/trip-service/internal/infrastructure/http"
 	"ride-sharing/services/trip-service/internal/infrastructure/repository"
 	"ride-sharing/services/trip-service/internal/service"
 	"ride-sharing/shared/env"
+	"syscall"
+	"time"
 )
 
 var (
@@ -27,7 +32,28 @@ func main() {
 		Handler: mux,
 	}
 
-	if err := server.ListenAndServe(); err != nil {
-		log.Printf("http server error: %v", err)
+	// Gracefully shutdown the server
+	serverErrors := make(chan error, 1)
+	go func() {
+		log.Printf("Server is running on %s\n", tripSVCAddr)
+		serverErrors <- server.ListenAndServe()
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	select {
+	case err := <-serverErrors:
+		log.Printf("Error starting the server: %v\n", err)
+	case sig := <-shutdown:
+		log.Printf("Server is shutting down due to %v\n", sig)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Println("Could not stop the server gracefully")
+			server.Close()
+		}
 	}
 }
