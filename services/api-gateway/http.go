6156113e2df7e41e074acee381b5a9318a6805a9
Fuzail Ahmed <fuzail1280@gmail.com>
2025-10-29 14:55:49 +0000

    feat: trip preview handler implementation

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -1,48 +1,47 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
+	"ride-sharing/services/api-gateway/grpc_clients"
 	"ride-sharing/shared/contracts"
 )
 
 func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 	var reqBody previewTripRequest
-
 	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
-		http.Error(w, "failed to parse json data", http.StatusBadRequest)
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
 		return
 	}
 
 	defer r.Body.Close()
 
-	// MARK: Validation
+	// validation
 	if reqBody.UserID == "" {
-		http.Error(w, "userid is required", http.StatusBadRequest)
+		http.Error(w, "user ID is required", http.StatusBadRequest)
 		return
 	}
 
-	// TODO: Call trip service
-	jsonBody, _ := json.Marshal(reqBody)
-	reader := bytes.NewReader(jsonBody)
-
-	resp, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+	// Why we need to create a new client for each connection:
+	// because if a service is down, we don't want to block the whole application
+	// so we create a new client for each connection
+	tripService, err := grpc_clients.NewTripServiceClient()
 	if err != nil {
-		log.Printf("%v\n", err)
-		http.Error(w, "route issue", http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
-
-	var respBody any
-	if err := json.NewDecoder(resp.Body).Decode(&respBody); err != nil {
-		http.Error(w, "failed to parse JSON data from trip service", http.StatusBadRequest)
-		return
+		log.Fatal(err)
 	}
 
-	response := contracts.APIResponse{Data: respBody}
+	// Don't forget to close the client to avoid resource leaks!
+	defer tripService.Close()
+
+	tripPreview, err := tripService.Client.PreviewTrip(r.Context(), reqBody.toProto())
+	if err != nil {
+		log.Printf("Failed to preview a trip: %v", err)
+		http.Error(w, "Failed to preview trip", http.StatusInternalServerError)
+		return
+	}
+
+	response := contracts.APIResponse{Data: tripPreview}
 
 	writeJSON(w, http.StatusCreated, response)
 }
