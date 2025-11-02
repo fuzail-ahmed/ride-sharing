5163d37b23e1c3e67dae5cd3599192439cac8f4a
Fuzail Ahmed <fuzail1280@gmail.com>
2025-11-02 09:48:26 +0000

    feat: add driver service

---
+++ services/driver-service/service.go
@@ -0,0 +1,22 @@
+package main
+
+import pb "ride-sharing/shared/proto/driver"
+
+type driverInMap struct {
+	Driver *pb.Driver
+
+	// index int
+	// TODO: route
+}
+
+type Service struct {
+	drivers []*driverInMap
+}
+
+func NewService() *Service {
+	return &Service{
+		drivers: make([]*driverInMap, 0),
+	}
+}
+
+// TODO: Register and unregister methods
