08339e1b4b63b7b809a843878b7b23c7b2a776ac
Fuzail Ahmed <fuzail1280@gmail.com>
2025-12-28 15:48:00 +0000

    feat: connecting to rabbitmq

--- go.mod
+++ go.mod
@@ -41,7 +41,7 @@ require (
 require (
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/mmcloughlin/geohash v0.10.0 // indirect
-	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.29.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
--- services/driver-service/main.go
+++ services/driver-service/main.go
@@ -8,12 +8,17 @@ import (
 	"os/signal"
 	"syscall"
 
+	"ride-sharing/shared/env"
+	"ride-sharing/shared/messaging"
+
 	grpcserver "google.golang.org/grpc"
 )
 
 var GrpcAddr = ":9092"
 
 func main() {
+	rabbitMqURI := env.GetString("RABBITMQ_URI", "amqp://guest:guest@rabbitmq:5672/")
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
@@ -30,6 +35,15 @@ func main() {
 	}
 
 	svc := NewService()
+
+	// RabbitMQ connection
+	rabbitmq, err := messaging.NewRabbitMQ(rabbitMqURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rabbitmq.Close()
+
+	log.Println("Starting RabbitMQ connection")
 
 	// Starting the gRPC server
 	grpcServer := grpcserver.NewServer()
